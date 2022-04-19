452b58348ec5f5d5495abf4b1486e70b3e395e5c
Henrik Stanley Mortensen <hsmx@novonordisk.com>
2022-04-19 20:22:02 +0200

    Update functions

---
+++ Source/Public/Set-AWSProfile.ps1
@@ -0,0 +1,59 @@
+function Set-AWSProfile {
+    <#
+    .SYNOPSIS
+        Set current shell session AWS Profile
+    .DESCRIPTION
+        Get a list of your configured AWS Profiles through the AWS CLI.
+        Select a profile from the list using FZF and finally export that profile to the $env:AWS_PROFILE environment variable.
+        This variable is respected by the AWS CLI, see Configuration settings and precedence in the AWS CLI documentation:
+        https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-quickstart.html
+
+    .EXAMPLE
+        PS C:\> awp
+        List all profiles in your .aws/config file for interactive selection.
+        Selected profile exported to $env:AWS_PROFILE environment variable
+
+    .EXAMPLE
+        PS C:\> awp myprofile
+        Pre-sort the profile list with the entered keyword.
+        If multiple profiles matches the keyword, user is prompted to select profile from a list.
+        If only a single match is found that profile is directly selected without further user prompts.
+        Selected profile exported to $env:AWS_PROFILE environment variable
+    .INPUTS
+        None
+        or
+        ProfileName
+    .OUTPUTS
+        $env:AWS_PROFILE
+    #>
+    [CmdletBinding()]
+    [Alias("awp")]
+    
+    Param(
+        [Parameter(
+            Position = 0,
+            Mandatory = $false,
+            ValueFromPipeline = $True
+        )]
+        [AllowEmptyString()]
+        [Alias('Name')]
+        [string[]]$ProfileName
+    )
+    
+    process {
+        if ($PSBoundParameters.ContainsKey('ProfileName')) {
+            $selectedProfile = aws configure list-profiles | fzf -q $ProfileName --select-1 --exit-0
+        }
+        else {
+            $selectedProfile = aws configure list-profiles | fzf
+        }
+    
+        If ($selectedProfile) {
+    
+            $env:AWS_PROFILE = $selectedProfile
+        }
+        else {
+            Write-Output "No Profile found / No Profile selected."
+        }
+    }
+}
---
+++ Source/Public/Set-AWSRegion.ps1
@@ -0,0 +1,58 @@
+Function Set-AWSRegion {
+    <#
+    .SYNOPSIS
+        Set default region to use for current shell session.
+    .DESCRIPTION
+        Get a list of allowed AWS Regions for your account through the AWS CLI.
+        Select a region from the list using FZF and finally export that profile to the $env:AWS_REGION environment variable.
+
+    .EXAMPLE
+        PS C:\> awr
+        List all allowed regions in your AWS account for interactive selection.
+        Selected region is exported to $env:AWS_REGION environment variable for default region.
+
+    .EXAMPLE
+        PS C:\> awp eu-central-1
+        Pre-sort the allowed regions list with the entered keyword.
+        If multiple regions matches the keyword, user is prompted to select profile from a list.
+        If only a single match is found that region is directly selected without further user prompts.
+        Selected region exported to $env:AWS_REGION environment variable
+    .INPUTS
+        None
+        or
+        ProfileName
+    .OUTPUTS
+        $env:AWS_REGION
+    #>
+    [CmdletBinding()]
+    [Alias("awr")]
+
+    Param(
+
+        [Parameter(
+            Position = 0,
+            Mandatory = $false,
+            ValueFromPipeline = $True
+        )]
+        [AllowEmptyString()]
+        [Alias('Region')]
+        [string[]]$RegionName
+    )
+
+    process {
+        if ($PSBoundParameters.ContainsKey('RegionName')) {
+            $selectedRegion = (aws ec2 describe-regions | ConvertFrom-Json).Regions.RegionName | fzf -q $RegionName --select-1 --exit-0
+        }
+        else {
+            $selectedRegion = (aws ec2 describe-regions | ConvertFrom-Json).Regions.RegionName | fzf
+        }
+        
+        If ($selectedRegion) {
+
+            $env:AWS_REGION = $selectedRegion
+        }
+        else {
+            Write-Output "No Region found / No Region selected."
+        }
+    }
+}
